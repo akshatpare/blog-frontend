7a208d0fb3c2fcba95127865557a2e1a909b3b01
ayan.shukla <ayan.shukla@flipkart.com>
2023-04-04 02:49:41 +0530

    updated indexpage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -4,7 +4,10 @@ import {useEffect, useState} from "react";
 export default function IndexPage() {
   const [posts,setPosts] = useState([]);
   useEffect(() => {
-    fetch('https://blog-backend-8yjaeo2nw-akshatpare.vercel.app/post').then(response => {
+    fetch('https://blog-backend-8yjaeo2nw-akshatpare.vercel.app/post', {
+      mode: 'cors',
+      credentials: 'include'
+    }).then(response => {
       response.json().then(posts => {
         setPosts(posts);
       });
